0a19770f4022dd91a11eb039f322862ea6a39259
Antoine LERNER <ant.lerner@gmail.com>
2016-05-27 14:12:20 +0200

    Ajout du module `Test2`, qui contient la classe `Spargel`, qui est un héritage de la classe `Wurz` du module `Test`.

--- test.rb
+++ test.rb
@@ -7,11 +7,25 @@ module Test
 		end
 
 		def say_hello_to name
-			"Hello #{name}, i'm #{@@name}"
+			"Hello #{name}, I'm #{@@name}"
 		end
 	end
-
 end
+
+module Test2
+	class Spargel < Test::Wurz
+		def stinking_pee
+			"I'll make you do a stinking pee"
+		end
+	end
+end
+
 
 wurz = Test::Wurz.new "Saucisse"		# => #<Test::Wurz:0x00000001de95f8>
 puts wurz.say_hello_to "David"			# => "Hello David, i'm Saucisse"
+
+puts "---------------------------------"
+
+spargel = Test2::Spargel.new "Asperge"
+puts spargel.say_hello_to "Jeannette"
+puts spargel.stinking_pee
