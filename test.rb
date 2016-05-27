10c460c07da5824dd973f09fa21bceaf3b07d874
Antoine LERNER <ant.lerner@gmail.com>
2016-05-27 08:21:50 +0200

    Modification de la classe Wurz

--- test.rb
+++ test.rb
@@ -15,12 +15,13 @@ module TakeBack
 		end
 		
 		def say_hello_to name
-			puts "Hello #{name}, i'm #{@@name}"
+			"Hello #{name}, i'm #{@@name}"
 		end
 	
 	end
 	
 end
 
-wurz = TakeBack::Wurz.new 'Saucisse'
-wurz.say_hello_to "David"
+wurz = TakeBack::Wurz.new 'Saucisse'	# => #<TakeBack::Wurz:0x00000001de95f8> 
+puts wurz.say_hello_to "David"		# => "Hello David, i'm Saucisse" 
+
