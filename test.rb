6facd9809d1ef7fc39e16f58966d8b5be97abac5
Antoine LERNER <ant.lerner@gmail.com>
2016-05-27 08:16:33 +0200

    Ajout de la class Wurz.

--- test.rb
+++ test.rb
@@ -5,4 +5,22 @@ t = true
 s = "ça marche bien"
 puts s if t
 
+module TakeBack
+	
+	class Wurz
+		@@name = "Wurz"
 
+		def initialize name
+			@@name = name
+		end
+		
+		def say_hello_to name
+			puts "Hello #{name}, i'm #{@@name}"
+		end
+	
+	end
+	
+end
+
+wurz = TakeBack::Wurz.new 'Saucisse'
+wurz.say_hello_to "David"
