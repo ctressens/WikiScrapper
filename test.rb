ad87376240be2ba7efb680b69ae0913be53703bd
Antoine LERNER <ant.lerner@gmail.com>
2016-05-27 10:07:24 +0200

    Tabulation sur la ligne 26

--- test.rb
+++ test.rb
@@ -6,22 +6,21 @@ s = "ça marche bien"
 puts s if t
 
 module TakeBack
-	
+
 	class Wurz
 		@@name = "Wurz"
 
 		def initialize name
 			@@name = name
 		end
-		
+
 		def say_hello_to name
 			"Hello #{name}, i'm #{@@name}"
 		end
-	
+
 	end
-	
+
 end
 
-wurz = TakeBack::Wurz.new 'Saucisse'	# => #<TakeBack::Wurz:0x00000001de95f8> 
-puts wurz.say_hello_to "David"		# => "Hello David, i'm Saucisse" 
-
+wurz = TakeBack::Wurz.new 'Saucisse'	# => #<TakeBack::Wurz:0x00000001de95f8>
+puts wurz.say_hello_to "David"			# => "Hello David, i'm Saucisse" 
