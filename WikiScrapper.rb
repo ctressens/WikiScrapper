b6bfaf5f0264e49727e720dc7e49ac9be18da4e2
Antoine LERNER <ant.lerner@gmail.com>
2016-05-28 12:15:57 +0200

    Pénis

--- WikiScrapper.rb
+++ WikiScrapper.rb
@@ -1,1 +1,19 @@
 #!/usr/bin/env ruby
+
+# ENTREES
+
+# Début
+print "URL de départ : "
+url_depart = gets.chomp
+while url_depart[0, 8] != "https://"
+    print "URL de départ : "
+    url_depart = gets.chomp
+end
+
+# Fin
+print "URL d'arrivée : "
+url_arrivee = gets.chomp
+while url_arrivee[0, 8] != "https://"
+    print "URL d'arrivée : "
+    url_arrivee = gets.chomp
+end
