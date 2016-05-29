8f7e31d945e81d6ddafce8f43f126f3923a03f13
takebackfr <ant.lerner@gmail.com>
2016-05-29 11:29:26 +0200

    .

--- WikiScrapper.rb
+++ WikiScrapper.rb
@@ -9,39 +9,40 @@ module WikiScrapper
         def initialize starting, ending
             @starting = starting
             @ending = ending
-
             @links = Array.new
         end
         def find_path
-
-        end
-        def get_links url = @starting
-            doc = Nokogiri::HTML(open(url))
-            @layer = Array.new
-            # @layers[@layers.length] = Array.new     # => Will be `lay`
-
-
-            doc.css('#mw-content-text a').each do |a|
-                href = a.attributes["href"].value
-                if href[0, 6] === "/wiki/"
-                    href["/wiki/"] = ""
-                else
-                    next
-                end
-                @layer[@layer.length] = href if !@layer.include? href
-                @links[@links.length] = href if !@links.include? href
-            end
+            get_links
+            puts "true" if @layer.include? format_url(@ending)
         end
 
         private
+            def get_links url = @starting
+                doc = Nokogiri::HTML(open(url))
+                @layer = Array.new
+                # @layers[@layers.length] = Array.new     # => Will be `lay`
 
+
+                doc.css('#mw-content-text a').each do |a|
+                    href = a.attributes["href"].value
+                    if href[0, 6] === "/wiki/"
+                        href["/wiki/"] = ""
+                    else
+                        next
+                    end
+                    @layer[@layer.length] = href if !@layer.include? href
+                    @links[@links.length] = href if !@links.include? href
+                end
+
+            end
             def format_url url
                 if url[0, 4] = "http"
-
+                    url[0, (url.index('/wiki/') + 6)] = ""
                 end
+                url
             end
     end
 end
 
-explor = WikiScrapper::Explorer.new 'https://fr.wikipedia.org/wiki/Ruby', 'https://fr.wikipedia.org/wiki/Python_(language)'
-explor.get_links
+explor = WikiScrapper::Explorer.new 'https://fr.wikipedia.org/wiki/Ruby', 'https://fr.wikipedia.org/wiki/Python_(langage)'
+explor.find_path
