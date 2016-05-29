790f7596500b75944068b5df818aff5e836a9e65
takebackfr <ant.lerner@gmail.com>
2016-05-29 11:11:34 +0200

    modif'

--- WikiScrapper.rb
+++ WikiScrapper.rb
@@ -6,16 +6,21 @@ require 'nokogiri'
 
 module WikiScrapper
     class Explorer
-    	def initialize starting, ending
+        def initialize starting, ending
             @starting = starting
             @ending = ending
 
-            @layers = Array.new
             @links = Array.new
+        end
+        def find_path
+
         end
         def get_links url = @starting
             doc = Nokogiri::HTML(open(url))
-            @layers[@layers.length] = Array.new
+            @layer = Array.new
+            # @layers[@layers.length] = Array.new     # => Will be `lay`
+
+
             doc.css('#mw-content-text a').each do |a|
                 href = a.attributes["href"].value
                 if href[0, 6] === "/wiki/"
@@ -23,11 +28,20 @@ module WikiScrapper
                 else
                     next
                 end
-                p href
+                @layer[@layer.length] = href if !@layer.include? href
+                @links[@links.length] = href if !@links.include? href
             end
         end
+
+        private
+
+            def format_url url
+                if url[0, 4] = "http"
+
+                end
+            end
     end
 end
 
-explor = WikiScrapper::Explorer.new 'https://fr.wikipedia.org/wiki/Ruby', 'https://fr.wikipedia.org/wiki/Python'
+explor = WikiScrapper::Explorer.new 'https://fr.wikipedia.org/wiki/Ruby', 'https://fr.wikipedia.org/wiki/Python_(language)'
 explor.get_links
