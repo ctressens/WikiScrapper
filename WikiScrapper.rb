7a5e3cd53e01afafcd9876a133365c75a4a059d9
takebackfr <ant.lerner@gmail.com>
2016-05-30 20:03:40 +0200

    From Scratch

--- WikiScrapper.rb
+++ WikiScrapper.rb
@@ -4,39 +4,46 @@ require 'uri'
 require 'open-uri'
 require 'nokogiri'
 
-module WikiScrapper
-    class Explorer
-        def initialize starting, ending
-            @starting = starting
-            @ending = ending
+module  Wiki
+
+    class PathFinder
+        def initialize starting_url, ending_url
+            @starting_url = starting_url
+            @ending_url = ending_url
         end
-        def find_path
-            get_links
+
+        def start_finding
+            if validate_url(@starting_url) && validate_url(@ending_url)
+                scan_page @starting_url
+            else
+                puts "Ooops, there is a problem. Please copy/paste the URLs from Wikipedia."
+            end
         end
 
         private
-            def get_links url = @starting
-                doc = Nokogiri::HTML(open(url))
-
-
-                doc.css('#mw-content-text a').each do |a|
-                    href = a.attributes["href"].value
-                    if href[0, 6] === "/wiki/"
-                        href["/wiki/"] = ""
-                    else
-                        next
-                    end
+            def validate_url url
+                if /^https:\/\/[a-z]{2}.wikipedia.org\/wiki\/.+$/ =~ url
+                    true
+                else
+                    false
                 end
-
             end
+
             def format_url url
-                if url[0, 4] = "http"
-                    url[0, (url.index('/wiki/') + 6)] = ""
+                if validate_url url
+                    url.sub(/^https:\/\/[a-z]{2}.wikipedia.org\/wiki\//, '')
+                else
+                    false
                 end
-                url
+            end
+
+            def scan_page url
+                document = Nokogiri::HTML(open(url))
+
             end
     end
+
 end
 
-explor = WikiScrapper::Explorer.new 'https://fr.wikipedia.org/wiki/Ruby', 'https://fr.wikipedia.org/wiki/Japonais'
-explor.find_path
+pathfind = Wiki::PathFinder.new "https://fr.wikipedia.org/wiki/Ruby", "https://fr.wikipedia.org/wiki/Python_(langage)"
+pathfind.start_finding
